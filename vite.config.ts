184fa84f9f1fa347777a44b96e5cefd0ca7e5717
ToruTamahashi <0ys25x4t725473s@gmail.com>
2025-01-07 17:08:04 +0900

    small update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
-// import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
 			insertTypesEntry: true,
 		}),
 		// CSSをJSにインジェクト
-		// libInjectCss(),
+		libInjectCss(),
 	],
 	build: {
 		lib: {
